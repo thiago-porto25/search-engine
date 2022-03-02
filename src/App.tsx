1f7e6fa9a2d25d6733ad3f0fc9bf4ff2771fa1c9
Thiago Porto <thiago.porto@d3.do>
2022-03-02 14:23:39 -0300

    feat: create and use constants file

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Home, Info, NotFound, Results } from 'pages';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import * as CONSTANTS from 'constants/index';
 
 const App = () => (
   <>
     <Header />
     <Routes>
-      <Route path="/">
+      <Route path={CONSTANTS.HOME}>
         <Route index element={<Home />} />
-        <Route path="results" element={<Results />} />
-        <Route path="page/:info" element={<Info />} />
+        <Route path={CONSTANTS.RESULTS} element={<Results />} />
+        <Route path={CONSTANTS.INFO} element={<Info />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
