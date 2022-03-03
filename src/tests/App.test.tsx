3a87db9a1716c5c9fd8f7cc1a1025246e5e0c399
Thiago Porto <thiago.porto@d3.do>
2022-03-03 12:18:24 -0300

    feat: make very simple tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders app logo', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Searchr/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders input', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByPlaceholderText(/Search.../);
   expect(linkElement).toBeInTheDocument();
 });
